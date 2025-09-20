8b3410fc53fc330c22d6bb61e4b011f81640a41b
ShadowShark99 <kadenwchan@gmail.com>
2025-09-20 18:12:29 -0500

    Initialize Database

---
+++ db/populatedb.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const {Client} = require("pg");
+
+const SQL=`
+  CREATE TABLE IF NOT EXISTS club_users(
+    email VARCHAR (255),
+    password VARCHAR (255), 
+    username VARCHAR (255),
+    member BOOLEAN DEFAULT FALSE
+  );
+
+  CREATE TABLE IF NOT EXISTS club_messages(
+    username VARCHAR (255),
+    title VARCHAR (255), 
+    message VARCHAR (255), 
+    time DATE NOT NULL DEFAULT CURRENT_DATE
+  );
+
+  INSERT INTO club_users (email, username, password)
+  VALUES
+    ('admin', 'admin', '67');
+
+  INSERT INTO club_messages (username, title, message)
+  VALUES
+    ('admin', 'Welcome', 'Will you join?');
+`;
+
+async function main (){
+  console.log("seeding...");
+  const client = new Client({
+    host: "localhost", // or wherever the db is hosted
+    user: "kaden",
+    database: "top_users",
+    port: 5432 // The default port
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("finish");
+}
+
+main();
